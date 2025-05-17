8604c86d804bc758e4e6f1a4c99660fda1e8384e
Alejandro Duran <alejandro.agustin.duran@gmail.com>
2025-05-16 22:27:30 -0300

    Implement project validation middleware and refactor task creation logic

--- Upstask_Backend/src/controllers/TaskController.ts
+++ Upstask_Backend/src/controllers/TaskController.ts
@@ -1,24 +1,18 @@
 import type { Request, Response } from "express";
-import Project from "../models/Project";
 import Task from "../models/Task";
+
 
 export class TaskController {
     static createTask = async (req: Request, res: Response): Promise<void> => {
 
-        const { projectId } = req.params;
-        const project = await Project.findById(projectId);
-        console.log(project);
-        if (!project) {
-            const error = new Error("Project not found");
-            res.status(404).json({ error: error.message })
-        }
         try {
             const task = new Task(req.body);
             //console.log(task);
-            task.project = project.id // Assign the project ID to the task
-            project.tasks.push(task.id); // Add the task ID to the project's tasks array
-            await task.save();
-            await project.save(); // Save the project with the new task ID
+            task.project = req.project.id // Assign the project ID to the task
+            req.project.tasks.push(task.id); // Add the task ID to the project's tasks array
+            /*             await task.save();
+                        await req.project.save(); // Save the project with the new task ID */
+            await Promise.allSettled([task.save(), req.project.save()])
             res.send("Task created successfully");
 
         } catch (error) {
---
+++ Upstask_Backend/src/middleware/project.ts
@@ -0,0 +1,26 @@
+import type { Request, Response, NextFunction } from "express";
+import Project, { Iproject } from "../models/Project";
+
+declare global{
+    namespace Express {
+        interface Request{
+            project: Iproject
+        }
+    }
+}
+
+export async function validateProjectExists(req: Request, res: Response, next: NextFunction) {
+    try {
+        const { projectId } = req.params;
+        const project = await Project.findById(projectId);
+        console.log(project);
+        if (!project) {
+            const error = new Error("Project not found");
+            res.status(404).json({ error: error.message })
+        }
+        req.project = project
+        next()
+    } catch (error) {
+        res.status(500).json({ error: "Error validating project" })
+    }
+}
