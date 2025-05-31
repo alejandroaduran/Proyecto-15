02a5c4f7b5ab4132d56f718248c8a1aaeb74840c
Alejandro Duran <alejandro.agustin.duran@gmail.com>
2025-05-31 02:52:12 -0300

    feat: enhance EditTaskData component with error handling and navigation for invalid task IDs

--- frontend_uptask/src/components/tasks/EditTaskData.tsx
+++ frontend_uptask/src/components/tasks/EditTaskData.tsx
@@ -1,4 +1,4 @@
-import { useLocation, useParams } from "react-router-dom"
+import { Navigate, useLocation, useParams } from "react-router-dom"
 import { useQuery } from "@tanstack/react-query"
 import { getTaskById } from "@/api/TaskAPI"
 import EditTaskModal from "./EditTaskModal"
@@ -17,14 +17,17 @@ export default function EditTaskData() {
     //console.log("Task ID to edit:", taskId)
 
     // Here you would typically use the taskId to fetch the task data
-    const { data } = useQuery({
+    // Here you would typically use the taskId to fetch the task data
+    const { data, error } = useQuery({
         queryKey: ['task', taskId],
         enabled: !!taskId,
+        retry: false,
         queryFn: async () => {
             if (!taskId) throw new Error("taskId is null");
             return getTaskById({ projectId, taskId });
         }
     })
+    if (error) return <Navigate to={"/404"} />
 
     if (data && taskId) return <EditTaskModal data={data} taskId={taskId} />
 }
