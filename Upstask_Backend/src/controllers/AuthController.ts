10496cf15917d1b644ab5b12bef9e25e6e321263
Alejandro Duran <alejandro.agustin.duran@gmail.com>
2025-06-03 00:25:42 -0300

    feat: implement password hashing and user existence check in account creation process

--- Upstask_Backend/src/controllers/AuthController.ts
+++ Upstask_Backend/src/controllers/AuthController.ts
@@ -1,12 +1,28 @@
 import type { Request, Response } from "express";
 import User from "../models/User";
+import { hashPassword } from "../utils/auth";
 
 export class AuthController {
     static createAccount = async (req: Request, res: Response) => {
         //    res.send("Creating account...");
         try {
-            const user = new User(req.body)
-            await user.save()
+            const { password, email } = req.body;
+
+            // Check if the user already exists
+            const userExists = await User.findOne({ email });
+            if (userExists) {
+                const error = new Error("User already exists.");
+                res.status(409).json({ error: error.message });
+                return;
+            }
+
+            //Create user
+            const user = new User(req.body);
+
+            //Hash the password
+            user.password = await hashPassword(password);
+
+            await user.save();
             res.send("Account created successfully! check your email to confirm your account.");
 
         } catch (error) {
