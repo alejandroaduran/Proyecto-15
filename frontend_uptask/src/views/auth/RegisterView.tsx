d4503693160bd01f43cdac58cc704cb75eb22c06
Alejandro Duran <alejandro.agustin.duran@gmail.com>
2025-06-06 00:35:03 -0300

    feat: add authentication views and layout for login and registration

---
+++ frontend_uptask/src/views/auth/RegisterView.tsx
@@ -0,0 +1,130 @@
+import { useForm } from "react-hook-form";
+import type { UserRegistrationForm } from "@/types/index";
+import ErrorMessage from "@/components/ErrorMessage";
+import { Link } from "react-router-dom";
+
+export default function RegisterView() {
+  
+  const initialValues: UserRegistrationForm = {
+    name: '',
+    email: '',
+    password: '',
+    password_confirmation: '',
+  }
+
+  const { register, handleSubmit, watch, reset, formState: { errors } } = useForm<UserRegistrationForm>({ defaultValues: initialValues });
+
+  const password = watch('password');
+
+  const handleRegister = (formData: UserRegistrationForm) => {}
+
+  return (
+    <>
+      <h1 className="text-5xl font-black text-white">Create Account</h1>
+      <p className="text-2xl font-light text-white mt-5">
+        Fill out the form to {''}
+        <span className=" text-fuchsia-500 font-bold"> create your account</span>
+      </p>
+
+      <form
+        onSubmit={handleSubmit(handleRegister)}
+        className="space-y-8 p-10  bg-white mt-10"
+        noValidate
+      >
+        <div className="flex flex-col gap-5">
+          <label
+            className="font-normal text-2xl"
+            htmlFor="email"
+          >Email</label>
+          <input
+            id="email"
+            type="email"
+            placeholder="Enter your email"
+            className="w-full p-3  border-gray-300 border"
+            {...register("email", {
+              required: "Email is required",
+              pattern: {
+                value: /\S+@\S+\.\S+/,
+                message: "Invalid email address",
+              },
+            })}
+          />
+          {errors.email && (
+            <ErrorMessage>{errors.email.message}</ErrorMessage>
+          )}
+        </div>
+
+        <div className="flex flex-col gap-5">
+          <label
+            className="font-normal text-2xl"
+          >Name</label>
+          <input
+            type="name"
+            placeholder="Enter your name"
+            className="w-full p-3  border-gray-300 border"
+            {...register("name", {
+              required: "Name is required",
+            })}
+          />
+          {errors.name && (
+            <ErrorMessage>{errors.name.message}</ErrorMessage>
+          )}
+        </div>
+
+        <div className="flex flex-col gap-5">
+          <label
+            className="font-normal text-2xl"
+          >Password</label>
+
+          <input
+            type="password"
+            placeholder="Enter your password"
+            className="w-full p-3  border-gray-300 border"
+            {...register("password", {
+              required: "Password is required",
+              minLength: {
+                value: 8,
+                message: 'Password must be at least 8 characters long'
+              }
+            })}
+          />
+          {errors.password && (
+            <ErrorMessage>{errors.password.message}</ErrorMessage>
+          )}
+        </div>
+
+        <div className="flex flex-col gap-5">
+          <label
+            className="font-normal text-2xl"
+          >Repeat Password</label>
+
+          <input
+            id="password_confirmation"
+            type="password"
+            placeholder="Repeat Password"
+            className="w-full p-3  border-gray-300 border"
+            {...register("password_confirmation", {
+              required: "Repeat Password is required",
+              validate: value => value === password || 'Passwords do not match'
+            })}
+          />
+
+          {errors.password_confirmation && (
+            <ErrorMessage>{errors.password_confirmation.message}</ErrorMessage>
+          )}
+        </div>
+
+        <input
+          type="submit"
+          value='Register'
+          className="bg-fuchsia-600 hover:bg-fuchsia-700 w-full p-3  text-white font-black  text-xl cursor-pointer"
+        />
+      </form>
+            <nav className="mt-10 flex flex-col space-y-4">
+              <Link to="/auth/login" className="text-fuchsia-500 hover:underline">
+                Already have an account? Login
+              </Link>
+            </nav>
+    </>
+  )
+}
